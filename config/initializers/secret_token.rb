3a258ec27db9f3222fe866403b32bd9fd20234c1
Pooja Parashar <pooja@appcircles.com>
2011-05-29 14:23:50 -0700

    new stocks apps

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stocks::Application.config.secret_token = 'f9f3e8a51775ddfbc763f092b9dcc7e4eb17b33c1efbfad8c42b16a124e10c24d5f300bfb0457fc7dc56955d5c04d1e0458fd449874d14c0368692ed021aa1f1'
